bc11a89cddba71e997e785a3061a0d1426e7e9a6
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-12-08 19:16:32 -0300

    Add files via upload

---
+++ src/controller/BuscaController.java
@@ -0,0 +1,171 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BuscaController 
+{
+	
+	public File validaSistema() 
+	{
+		
+		String system = System.getProperty("os.name").toLowerCase(), path = null;
+               
+        if (system.contains("win")) 
+        {
+        	path = "C:"+ File.separator + "TEMP";
+        } 
+        else if (system.contains("nix") || system.contains("nux"))
+        {
+            path = File.separator + "tmp";
+        }
+        
+        File file = new File(path, "trades.json");
+        
+		return file;
+		
+	}
+	
+	public boolean exists(String ids, int market) 
+	{
+		
+		String marketStr = String.valueOf(market);
+		String[] vetIds = ids.split("[-\\n]+");
+		int tamanhoVet = vetIds.length;
+		
+		for(int i = 0; i < tamanhoVet; i++) 
+		{
+			vetIds[i] = vetIds[i].trim();
+			
+			if(vetIds[i].equals(marketStr)) 
+			{
+				return true;
+			}
+		}
+
+		return false;
+	
+	}
+	
+	public String retornaID() throws IOException
+	{
+		
+		StringBuilder stringBuilder = new StringBuilder();
+        File file = validaSistema();
+
+        if (file.exists() && file.isFile())
+        {
+        	 
+        	FileInputStream r = new FileInputStream(file);
+        	InputStreamReader reader = new InputStreamReader(r); 
+        	BufferedReader buffer = new BufferedReader(reader);
+        	int divisor = 0;
+        	String linha;
+			
+        	while ((linha = buffer.readLine()) != null)
+        	{
+        		
+        		String[] vetLinha = linha.split("}");
+        		int size = vetLinha.length;
+        		
+        		for(int i = 0; i < size; i++) 
+        		{
+        			
+        			String[]vetSec = vetLinha[i].split(",");
+					
+        			int tamanho = vetSec.length;
+				
+        			for(int j = 0; j < tamanho - 1; j++) 
+        			{
+        				if (vetSec[j].contains("id")) 
+        				{
+        					String id= vetSec[j].substring(vetSec[j].indexOf(":") + 1).replace("\"", "").trim();
+					
+        					if( divisor < 4) 
+        					{
+        						stringBuilder.append(id).append(" - ");
+        						divisor=divisor+1;
+        					} 
+        					else
+        					{
+        						stringBuilder.append(id).append("\n");
+        						divisor=0;
+        					}
+        				}
+        			}
+        		}
+				
+        	}
+			
+			buffer.close();
+			r.close();
+			reader.close();
+			stringBuilder.setLength(stringBuilder.length() - 1);
+
+         }
+		
+         return stringBuilder.toString();	
+	
+}
+
+	public String buscaDados(int market) throws IOException 
+	{
+		
+		String marketStr = String.valueOf(market);
+		String response = null;
+        File file = validaSistema();
+		
+		if (file.exists() && file.isFile()) 
+		{
+			
+			FileInputStream r = new FileInputStream(file);
+			InputStreamReader reader = new InputStreamReader (r); 
+			BufferedReader buffer = new BufferedReader (reader);
+
+			String linha = buffer.readLine();
+			String funds = null;
+			String created = null;
+			
+			String[] vetLinha = linha.split("}");
+			int size = vetLinha.length;
+			
+			for(int i = 0; i < size; i++) 
+			{
+					
+				if(vetLinha[i].contains(marketStr)) 
+				{
+					
+					String[] vetSec = vetLinha[i].split(",");		
+					int size2 = vetSec.length;
+	
+					for(int j = 0; j < size2 ; j++)
+					{
+						if (vetSec[j].contains("funds")) 
+						{
+							funds = vetSec[j].substring(vetSec[j].indexOf(":") + 1).replace("\"", "").trim();
+						}
+						if (vetSec[j].contains("created")) 
+						{
+							created = vetSec[j].substring(vetSec[j].indexOf(":") + 1).replace("\"", "").replace("]", "").trim();	  
+							created = created.replace("-", "/").replace("T", " às ").replace("Z", "").trim();
+						}
+							
+						response = ( "ID "+ marketStr + " \nFunds = " + funds + "\nCriado em " + created);
+						
+					}
+				}
+			}
+                
+			buffer.close();
+			r.close();
+			reader.close();
+				
+		}
+		
+		return response;
+		
+	}
+}
---
+++ src/view/MenuPrincipal.java
@@ -0,0 +1,44 @@
+//Isabel Costa Lavorenti
+
+package view;
+
+import java.io.IOException;
+import controller.BuscaController;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class MenuPrincipal
+{
+	
+	public static void main(String[] args) throws IOException 
+	{
+		
+		boolean idExiste = false;
+		int market = 0;
+        BuscaController controller = new BuscaController();
+        String ids = controller.retornaID();
+           
+        while(idExiste == false)
+        {
+        	JTextArea textArea = new JTextArea("                                                  Localizador de ID\n\nIDs cadastrados:\n" + ids + "\n\nInsira um ID:");
+        	textArea.setWrapStyleWord(true);
+        	textArea.setLineWrap(true);
+        	textArea.setEditable(false); 
+        	JScrollPane scrollPane = new JScrollPane(textArea);
+        	scrollPane.setPreferredSize(new java.awt.Dimension(400,250));
+        	market = Integer.parseInt(JOptionPane.showInputDialog(null, scrollPane, "Market", JOptionPane.PLAIN_MESSAGE));
+        	idExiste = controller.exists(ids, market);
+        	   
+        	if(!idExiste) 
+        	{
+        		JOptionPane.showMessageDialog(null, market +" não é um ID valido!\nInsira novamente.");
+        	}
+        	   
+         }
+          
+         String dados = controller.buscaDados(market);
+         JOptionPane.showMessageDialog(null, dados);
+           
+	}
+}
