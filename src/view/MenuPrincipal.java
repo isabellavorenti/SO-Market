2d51149e37f87f1cf1700b68cc42d0f2d7c135e2
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-12-08 19:26:46 -0300

    Update MenuPrincipal.java

--- src/view/MenuPrincipal.java
+++ src/view/MenuPrincipal.java
@@ -16,29 +16,29 @@ public class MenuPrincipal
 		
 		boolean idExiste = false;
 		int market = 0;
-        BuscaController controller = new BuscaController();
-        String ids = controller.retornaID();
+        	BuscaController controller = new BuscaController();
+       	 	String ids = controller.retornaID();
            
-        while(idExiste == false)
-        {
-        	JTextArea textArea = new JTextArea("                                                  Localizador de ID\n\nIDs cadastrados:\n" + ids + "\n\nInsira um ID:");
-        	textArea.setWrapStyleWord(true);
-        	textArea.setLineWrap(true);
-        	textArea.setEditable(false); 
-        	JScrollPane scrollPane = new JScrollPane(textArea);
-        	scrollPane.setPreferredSize(new java.awt.Dimension(400,250));
-        	market = Integer.parseInt(JOptionPane.showInputDialog(null, scrollPane, "Market", JOptionPane.PLAIN_MESSAGE));
-        	idExiste = controller.exists(ids, market);
-        	   
-        	if(!idExiste) 
+        	while(idExiste == false)
         	{
-        		JOptionPane.showMessageDialog(null, market +" não é um ID valido!\nInsira novamente.");
-        	}
+        		JTextArea textArea = new JTextArea("                                                  Localizador de ID\n\nIDs cadastrados:\n" + ids + "\n\nInsira um ID:");
+        		textArea.setWrapStyleWord(true);
+        		textArea.setLineWrap(true);
+        		textArea.setEditable(false); 
+        		JScrollPane scrollPane = new JScrollPane(textArea);
+        		scrollPane.setPreferredSize(new java.awt.Dimension(400,250));
+        		market = Integer.parseInt(JOptionPane.showInputDialog(null, scrollPane, "Market", JOptionPane.PLAIN_MESSAGE));
+        		idExiste = controller.exists(ids, market);
         	   
-         }
+        		if(!idExiste) 
+        		{
+        			JOptionPane.showMessageDialog(null, market +" não é um ID válido!\nInsira novamente.");
+        		}
+        	   
+         	}
           
-         String dados = controller.buscaDados(market);
-         JOptionPane.showMessageDialog(null, dados);
+        	String dados = controller.buscaDados(market);
+         	JOptionPane.showMessageDialog(null, dados);
            
 	}
 }
