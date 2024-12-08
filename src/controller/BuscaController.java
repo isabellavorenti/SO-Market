1a804c82a6f1cff09f00565aec3aafb0fa738417
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-12-08 19:24:06 -0300

    Update BuscaController.java

--- src/controller/BuscaController.java
+++ src/controller/BuscaController.java
@@ -14,19 +14,17 @@ public class BuscaController
 		
 		String system = System.getProperty("os.name").toLowerCase(), path = null;
                
-        if (system.contains("win")) 
-        {
-        	path = "C:"+ File.separator + "TEMP";
-        } 
-        else if (system.contains("nix") || system.contains("nux"))
-        {
-            path = File.separator + "tmp";
-        }
+        	if (system.contains("win")) 
+        	{
+        		path = "C:"+ File.separator + "TEMP";
+     	 	} 
+    	    	else if (system.contains("nix") || system.contains("nux"))
+        	{
+           		path = File.separator + "tmp";
+        	}
         
-        File file = new File(path, "trades.json");
-        
+        	File file = new File(path, "trades.json");
 		return file;
-		
 	}
 	
 	public boolean exists(String ids, int market) 
@@ -54,69 +52,69 @@ public class BuscaController
 	{
 		
 		StringBuilder stringBuilder = new StringBuilder();
-        File file = validaSistema();
+        	File file = validaSistema();
 
-        if (file.exists() && file.isFile())
-        {
-        	 
-        	FileInputStream r = new FileInputStream(file);
-        	InputStreamReader reader = new InputStreamReader(r); 
-        	BufferedReader buffer = new BufferedReader(reader);
-        	int divisor = 0;
-        	String linha;
-			
-        	while ((linha = buffer.readLine()) != null)
+        	if (file.exists() && file.isFile())
         	{
-        		
-        		String[] vetLinha = linha.split("}");
-        		int size = vetLinha.length;
-        		
-        		for(int i = 0; i < size; i++) 
+        	 
+        		FileInputStream r = new FileInputStream(file);
+        		InputStreamReader reader = new InputStreamReader(r); 
+        		BufferedReader buffer = new BufferedReader(reader);
+        		int divisor = 0;
+        		String linha;
+			
+        		while ((linha = buffer.readLine()) != null)
         		{
-        			
-        			String[]vetSec = vetLinha[i].split(",");
-					
-        			int tamanho = vetSec.length;
-				
-        			for(int j = 0; j < tamanho - 1; j++) 
+        		
+        			String[] vetLinha = linha.split("}");
+        			int size = vetLinha.length;
+        		
+        			for(int i = 0; i < size; i++) 
         			{
-        				if (vetSec[j].contains("id")) 
-        				{
-        					String id= vetSec[j].substring(vetSec[j].indexOf(":") + 1).replace("\"", "").trim();
+        			
+        				String[]vetSec = vetLinha[i].split(",");
 					
-        					if( divisor < 4) 
+        				int tamanho = vetSec.length;
+				
+        				for(int j = 0; j < tamanho - 1; j++) 
+        				{
+        					if (vetSec[j].contains("id")) 
         					{
-        						stringBuilder.append(id).append(" - ");
-        						divisor=divisor+1;
-        					} 
-        					else
-        					{
-        						stringBuilder.append(id).append("\n");
-        						divisor=0;
+        						String id= vetSec[j].substring(vetSec[j].indexOf(":") + 1).replace("\"", "").trim();
+					
+        						if(divisor < 4) 
+        						{
+        							stringBuilder.append(id).append(" - ");
+        							divisor += 1;
+        						} 
+        						else
+        						{
+        							stringBuilder.append(id).append("\n");
+        							divisor = 0;
+        						}
         					}
         				}
         			}
-        		}
 				
-        	}
+        		}
 			
 			buffer.close();
 			r.close();
 			reader.close();
 			stringBuilder.setLength(stringBuilder.length() - 1);
 
-         }
+         	}
 		
-         return stringBuilder.toString();	
+         	return stringBuilder.toString();	
 	
-}
+	}
 
 	public String buscaDados(int market) throws IOException 
 	{
 		
 		String marketStr = String.valueOf(market);
 		String response = null;
-        File file = validaSistema();
+        	File file = validaSistema();
 		
 		if (file.exists() && file.isFile()) 
 		{
